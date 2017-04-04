4410d9ffe91c512b1868a657c108ce7b0ab9fc51
NonError87 <5gav2zrd@gmail.com>
2017-04-04 18:08:23 +0800

    pypi.py
    。

---
+++ pypi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+for command in [
+    #"pip install beautifulsoup4",
+    #"pip install html5lib"
+    #"pip install utils",
+    #"pip install db",
+    #"pip install request",
+    "pip install http",
+    #"pip install webapi",
+    #"pip install httpserver"
+    "easy_install web.py"
+]:
+    os.system(command)
