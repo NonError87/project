49533bca95150af286ba4ce7e4b0e4ca2d75d6db
NonError87 <5gav2zrd@gmail.com>
2017-04-04 18:09:43 +0800

    Update pypi.py

--- pypi.py
+++ pypi.py
@@ -3,14 +3,14 @@
 import os
 
 for command in [
-    #"pip install beautifulsoup4",
-    #"pip install html5lib"
-    #"pip install utils",
-    #"pip install db",
-    #"pip install request",
+    "pip install beautifulsoup4",
+    "pip install html5lib"
+    "pip install utils",
+    "pip install db",
+    "pip install request",
     "pip install http",
-    #"pip install webapi",
-    #"pip install httpserver"
+    "pip install webapi",
+    "pip install httpserver"
     "easy_install web.py"
 ]:
     os.system(command)
