cf28225bce5956266a9b6ba7e330c726b062dca6
NonError87 <5gav2zrd@gmail.com>
2017-05-13 11:15:04 +0800

    Update pypi.py

--- pypi.py
+++ pypi.py
@@ -4,13 +4,7 @@ import os
 
 for command in [
     "pip install beautifulsoup4",
-    "pip install html5lib"
-    "pip install utils",
-    "pip install db",
-    "pip install request",
-    "pip install http",
-    "pip install webapi",
-    "pip install httpserver"
-    "easy_install web.py"
+    "pip install html5lib",
+    "easy_install pillow"
 ]:
     os.system(command)
