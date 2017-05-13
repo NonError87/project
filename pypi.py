41e950c8dc273dc315190b186dfd0ff394302f74
NonError87 <5gav2zrd@gmail.com>
2017-05-13 21:58:07 +0800

    Update pypi.py

--- pypi.py
+++ pypi.py
@@ -5,7 +5,9 @@ import os
 for command in [
     "pip install beautifulsoup4",
     "pip install html5lib",
-    "easy_install pillow"
+    "easy_install pillow",
+    "pip install flask",
+    "pip install matplotlib"
     #install pywin32apiu
 ]:
     os.system(command)
